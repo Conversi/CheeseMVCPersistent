11238e3cbc021adb0f6e1007beaa66141016e237
Brian D. Conversi <bdconversi@yahoo.com>
2018-12-20 16:11:47 -0600

    Oops forgot initial commit

--- src/CheeseMVC/Controllers/CheeseController.cs
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using CheeseMVC.ViewModels;
 using CheeseMVC.Data;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace CheeseMVC.Controllers
 {
@@ -16,17 +17,17 @@ namespace CheeseMVC.Controllers
             context = dbContext;
         }
 
-        // GET: /<controller>/
+        //GET: /<controller>/
         public IActionResult Index()
         {
-            List<Cheese> cheeses = context.Cheeses.ToList();
+            IList<Cheese> cheeses = context.Cheeses.Include(c => c.Category).ToList();
 
             return View(cheeses);
         }
 
         public IActionResult Add()
         {
-            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel();
+            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel(context.Categories.ToList());
             return View(addCheeseViewModel);
         }
 
@@ -35,13 +36,16 @@ namespace CheeseMVC.Controllers
         {
             if (ModelState.IsValid)
             {
-                // Add the new cheese to my existing cheeses
-                Cheese newCheese = new Cheese
-                {
-                    Name = addCheeseViewModel.Name,
-                    Description = addCheeseViewModel.Description,
-                    Type = addCheeseViewModel.Type
-                };
+                //retrieve the category of cheese by the id that is submitted when the user chooses a selectlist item of categories from the categories selectlist.
+                    CheeseCategory newCheeseCategory = context.Categories.Single(c => c.ID == addCheeseViewModel.CategoryID);
+
+                //Add the new cheese to my existing cheeses
+                    Cheese newCheese = new Cheese
+                    {
+                        Name = addCheeseViewModel.Name,
+                        Description = addCheeseViewModel.Description,
+                        Category = newCheeseCategory
+                    };
 
                 context.Cheeses.Add(newCheese);
                 context.SaveChanges();
